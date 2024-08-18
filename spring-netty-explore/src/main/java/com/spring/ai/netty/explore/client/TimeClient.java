275dbfa103cd5ba2c04e3be6e8a66230427ab148
coderliaolong <coderliaolong@outlook.com>
2024-08-18 13:21:31 +0800

    【netty】echo、time server

---
+++ spring-netty-explore/src/main/java/com/spring/ai/netty/explore/client/TimeClient.java
@@ -0,0 +1,41 @@
+package com.spring.ai.netty.explore.client;
+
+import com.spring.ai.netty.explore.handler.TimeClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author ll
+ * @since 2024-08-17 16:15
+ */
+public class TimeClient {
+    public static void main(String[] args) throws InterruptedException {
+        String host = "localhost";
+        int port = 1111;
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(workerGroup)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            // Start the client.
+            ChannelFuture f = bootstrap.connect(host, port).sync(); // (5)
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
