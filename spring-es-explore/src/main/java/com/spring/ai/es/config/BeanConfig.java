4a6ed9fc9432fab403c813b6efe1a1ad684a81f8
coderliaolong <coderliaolong@outlook.com>
2024-10-27 16:35:50 +0800

    添加ElasticSearchClient测试用例

---
+++ spring-es-explore/src/main/java/com/spring/ai/es/config/BeanConfig.java
@@ -0,0 +1,5 @@
+package com.spring.ai.es.config;/**
+ * @author  ll
+ * @since 2024-10-27 11:10
+ */public class BeanConfig {
+}
---
+++ spring-es-explore/src/main/java/com/spring/ai/es/entity/TestIndex.java
@@ -0,0 +1,5 @@
+package com.spring.ai.es.entity;/**
+ * @author  ll
+ * @since 2024-10-27 11:00
+ */public class TestIndex {
+}
